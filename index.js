df7160fc19b3d0d729af1b2cd294fdf60adbe030
wuguohao <1142279752@qq.com>
2023-11-28 11:30:11 +0800

    fix

---
+++ index.js
@@ -0,0 +1,3 @@
+const { handleVue } = require('./src/index'); // 导入扩展模块
+
+module.exports = { handleVue };
--- src/index.js
+++ src/index.js
@@ -4,15 +4,10 @@ const { exit } = require('node:process');
 const cliProgress = require('cli-progress');
 const { getVueSourceFilePaths } = require('./utils/common.js');
 const { transformVue } = require('./core.js');
-const configFilePath = path.resolve(path.join(process.cwd(), 'config.js'));
 
-if (!fs.existsSync(configFilePath)) {
-  console.log('配置文件不存在');
-  exit(1);
-} else {
-  const config = require(configFilePath);
-  const { input, callBackFn } = config;
-  const filePathList = getVueSourceFilePaths(input);
+const handleVue = (filePath, callBackFn) => {
+  if (!filePath) return;
+  const filePathList = getVueSourceFilePaths(filePath);
   if (input) {
     const bar = new cliProgress.SingleBar(
       {
@@ -33,4 +28,35 @@ if (!fs.existsSync(configFilePath)) {
       }
     });
   }
-}
+};
+// if (!fs.existsSync(configFilePath)) {
+//   console.log('配置文件不存在');
+//   exit(1);
+// } else {
+//   const config = require(configFilePath);
+//   const { input, callBackFn } = config;
+//   const filePathList = getVueSourceFilePaths(input);
+//   if (input) {
+//     const bar = new cliProgress.SingleBar(
+//       {
+//         format: `执行进度: [{bar}] {percentage}% {value}/{total}`,
+//       },
+//       cliProgress.Presets.shades_classic
+//     );
+//     let length = filePathList.length;
+//     bar.start(length, 0);
+//     filePathList.forEach(async (filePath) => {
+//       const sourceCode = fs.readFileSync(filePath, 'utf8');
+//       const { code } = await transformVue(sourceCode, filePath, callBackFn);
+//       fs.writeFileSync(filePath, code, 'utf8');
+//       bar.increment();
+//       length--;
+//       if (length === 0) {
+//         bar.stop();
+//       }
+//     });
+//   }
+// }
+module.exports = {
+  handleVue,
+};
